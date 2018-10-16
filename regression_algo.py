552728d700154524e9ee84c4076fbc95349bb1ee
Harsh <deadspaceharsh@gmail.com>
2018-10-16 23:51:59 +0530

    adjusted quandl data

--- regression_algo.py
+++ regression_algo.py
@@ -14,9 +14,10 @@ forecast_col = 'Close'
 
 data_frame_needed.fillna(-99999 , inplace = True)
 
-forecast_out = int(math.ceil(0.001*len(data_frame_needed)))
+forecast_out = int(math.ceil(0.0001*len(data_frame_needed)))
 
 data_frame_needed['label'] = data_frame_needed[forecast_col].shift(-forecast_out)
+data_frame_needed.dropna(inplace = True)
 
 print(data_frame_needed.head())
 
