7ca1207ba401447f29939d5fc1aecc5512e9e6d9
Harsh <deadspaceharsh@gmail.com>
2018-10-16 23:47:12 +0530

    precise close values

--- regression_algo.py
+++ regression_algo.py
@@ -16,8 +16,9 @@ data_frame_needed.fillna(-99999 , inplace = True)
 
 forecast_out = int(math.ceil(0.1*len(data_frame_needed)))
 
+data_frame_needed['label'] = data_frame_needed[forecast_col].shift(-forecast_out)
 
-
+print(data_frame_needed.head())
 
 
 
