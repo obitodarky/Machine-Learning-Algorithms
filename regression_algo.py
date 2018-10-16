fb74e8806f628021168269d2581f9193f487d615
Harsh <deadspaceharsh@gmail.com>
2018-10-16 23:48:36 +0530

    improvements in algo

--- regression_algo.py
+++ regression_algo.py
@@ -14,7 +14,7 @@ forecast_col = 'Close'
 
 data_frame_needed.fillna(-99999 , inplace = True)
 
-forecast_out = int(math.ceil(0.1*len(data_frame_needed)))
+forecast_out = int(math.ceil(0.001*len(data_frame_needed)))
 
 data_frame_needed['label'] = data_frame_needed[forecast_col].shift(-forecast_out)
 
